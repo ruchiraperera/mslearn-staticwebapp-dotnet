04f97a16e8da3bbd12aeb62e3f948a104fd136a1
ruchira perera <ruchira.perena@consid.se>
2022-06-30 00:00:11 +0530

    change

--- Api/Function1.cs
+++ Api/Function1.cs
@@ -10,26 +10,22 @@ using Newtonsoft.Json;
 
 namespace Api
 {
-    public static class Function1
+    public class Function1
     {
+        private readonly IProductData _productData;
+
+        public Function1(IProductData productData)
+        {
+            _productData = productData;
+        }
         [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "products")] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            var product = await _productData.GetProducts();
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(product);
         }
     }
 }
